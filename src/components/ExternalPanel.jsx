b893c25139680a758f091a34a28fd7afb19c74ac
marwaelhajouji <marwa.elhajouji@gmail.com>
2023-08-31 02:45:07 +0200

    améliorerExternalPanels

--- src/components/ExternalPanel.jsx
+++ src/components/ExternalPanel.jsx
@@ -1,41 +1,63 @@
 import React, { useState, useEffect } from "react"
-import axios from "axios" // N'oubliez pas d'importer axios
+import axios from "axios"
 import "../styles/externalPanel.css"
 import { registerAction } from "../api/elevatorApi"
-import { calculateArrivalTime, calculateTotalSteps, calculateIntervalTime } from "../utils/calculator"
+import {
+  calculateArrivalTime,
+  calculateTotalSteps,
+  calculateIntervalTime
+} from "../utils/calculator"
 
 const ExternalPanel = ({ floors, title, allowedFloors = floors }) => {
   const [clickedButtons, setClickedButtons] = useState({})
-  const [currentAscFloor, setCurrentAscFloor] = useState(3) //
+  const [currentAscFloor, setCurrentAscFloor] = useState(3)
+  const [floorQueue, setFloorQueue] = useState([])
 
-  const moveToFloor = (floor) => {
-    setClickedButtons({ ...clickedButtons, [floor]: true }) // Allume le bouton
-
-    const arrivalTime = calculateArrivalTime(currentAscFloor, floor) // Temps total pour atteindre l'étage cible
-    const totalSteps = calculateTotalSteps(currentAscFloor, floor); // Nombre total d'étages à parcourir
-    const intervalTime = calculateIntervalTime(arrivalTime, totalSteps); // Temps pour atteindre chaque étage intermédiaire
-
-    let nextFloor = currentAscFloor
-
-    const interval = setInterval(() => {
-      nextFloor = nextFloor < floor ? nextFloor + 1 : nextFloor - 1
-      setCurrentAscFloor(nextFloor) // Met à jour l'étage actuel
-
-      if (nextFloor === floor) {
-        clearInterval(interval) // Arrête l'intervalle une fois l'étage cible atteint
-        setClickedButtons({ ...clickedButtons, [floor]: false }) // Éteint le bouton
-        // Ici, vous pouvez également signaler que les portes sont ouvertes si nécessaire.
-      }
-    }, intervalTime)
+  const addFloorToQueue = (floor) => {
+    setFloorQueue((prev) => [...prev, floor])
   }
 
+  useEffect(() => {
+    if (floorQueue.length === 0) return
+
+    const moveToNextFloor = () => {
+      const nextFloor = floorQueue[0]
+      const remainingFloors = floorQueue.slice(1)
+
+      const arrivalTime = calculateArrivalTime(currentAscFloor, nextFloor)
+      const totalSteps = calculateTotalSteps(currentAscFloor, nextFloor)
+      const intervalTime = calculateIntervalTime(arrivalTime, totalSteps)
+
+      let intermediateFloor = currentAscFloor
+
+      const interval = setInterval(() => {
+        if (intermediateFloor < nextFloor) {
+          intermediateFloor += 1
+        } else {
+          intermediateFloor -= 1
+        }
+        setCurrentAscFloor(intermediateFloor)
+
+        if (intermediateFloor === nextFloor) {
+          clearInterval(interval)
+          setTimeout(() => {
+            setFloorQueue(remainingFloors)
+            setClickedButtons({ ...clickedButtons, [nextFloor]: false }) // Turn off the button
+          }, 500) // attend 5s
+        }
+      }, intervalTime)
+    }
+
+    moveToNextFloor()
+  }, [floorQueue])
+
   const handleRequestForFloor = async (floor) => {
-    // Ici, on allume le bouton et on déclenche l'ascenseur
     if (!allowedFloors.includes(floor)) {
       console.log("This elevator doesn't stop at this floor.")
       return
     }
-    moveToFloor(floor)
+    setClickedButtons({ ...clickedButtons, [floor]: true })
+    addFloorToQueue(floor)
 
     const dataToSend = {
       actionType: "external_call",
